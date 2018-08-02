5bdfa117351f611b1bdf561703d3559f670bb373
Laxmi Tiruvuri (Inconsulting Inc.) <v-latiru@microsoft.com>
2018-08-02 14:05:50 -0700

    added classes

---
+++ abstract2/abstract2/Boss.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstract2
+{
+    public class Boss : Employee
+    {
+        public Boss(string name) : base(name)
+        {
+
+        }
+        public override int GetSalary()
+        {
+            return 1000000;
+        }
+    }
+}
